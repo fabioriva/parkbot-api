d622f8e1fa8e5a800d4088b005894f06b6ea145c
Fabio Riva <f.riva@sotefin.ch>
2023-09-20 09:35:12 +0100

    aps/il main function call

--- aps/il/aminadav16/index.js
+++ aps/il/aminadav16/index.js
@@ -1,6 +1,6 @@
 const def = require('./def')
 const obj = require('./obj')
 const str = require('./str')
-const main = require('../../lib/main')
+const main = require('../../../lib/main')
 
 main(def, obj, str)
