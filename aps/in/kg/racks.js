03b2389aa816fbdb0109981c2f4ab4a6ab499830
Fabio Riva <f.riva@sotefin.ch>
2023-10-06 13:46:07 +0100

    refactor: new racks array for aps/in/kg

--- aps/in/kg/racks.js
+++ aps/in/kg/racks.js
@@ -1,9 +1,8 @@
-const { ab, eb } = require('./obj')
+const { ab, eb, merkers } = require('./obj')
 const {
   S7_521_1BL00_0AB0,
   S7_521_1BH00_0AB0,
   S7_522_1BH01_0AB0,
-  // S7_522_1BL01_0AB0,
   S7_526_1BH00_0AB0,
   S7_526_2BF00_0AB0,
   S7_131_6BF00_0BA0,
@@ -14,8 +13,6 @@ const {
 
 const rack1 = {
   nr: 1,
-  serie: 'et200m',
-  title: 'Main',
   cards: [
     new S7_526_1BH00_0AB0(1, eb.slice(27, 29)),
     new S7_526_2BF00_0AB0(2, ab.slice(16, 17)),
@@ -26,8 +23,6 @@ const rack1 = {
 
 const rack2 = {
   nr: 2,
-  serie: 'et200m',
-  title: 'LS1',
   cards: [
     new S7_526_1BH00_0AB0(1, eb.slice(29, 31)),
     new S7_526_2BF00_0AB0(2, ab.slice(17, 18)),
@@ -38,8 +33,6 @@ const rack2 = {
 
 const rack3 = {
   nr: 3,
-  serie: 'et200s',
-  title: 'KKP1',
   cards: [
     new S7_136_6BA00_0CA0(1, eb[31]),
     new S7_136_6BD00_0CA0(2, ab[18]),
@@ -51,8 +44,6 @@ const rack3 = {
 
 const rack4 = {
   nr: 4,
-  serie: 'et200s',
-  title: 'SH1',
   cards: [
     new S7_136_6BA00_0CA0(1, eb[32]),
     new S7_136_6BD00_0CA0(2, ab[19]),
@@ -67,8 +58,6 @@ const rack4 = {
 
 const rack5 = {
   nr: 5,
-  serie: 'et200s',
-  title: 'KKE1',
   cards: [
     new S7_131_6BF00_0BA0(1, eb[12]),
     new S7_131_6BF00_0BA0(2, eb[13]),
@@ -82,8 +71,6 @@ const rack5 = {
 
 const rack6 = {
   nr: 6,
-  serie: 'et200m',
-  title: 'LS2',
   cards: [
     new S7_526_1BH00_0AB0(1, eb.slice(33, 35)),
     new S7_526_2BF00_0AB0(2, ab.slice(20, 21)),
@@ -94,8 +81,6 @@ const rack6 = {
 
 const rack7 = {
   nr: 7,
-  serie: 'et200s',
-  title: 'KKP2',
   cards: [
     new S7_136_6BA00_0CA0(1, eb[35]),
     new S7_136_6BD00_0CA0(2, ab[21]),
@@ -107,8 +92,6 @@ const rack7 = {
 
 const rack8 = {
   nr: 8,
-  serie: 'et200s',
-  title: 'SH2',
   cards: [
     new S7_136_6BA00_0CA0(1, eb[36]),
     new S7_136_6BD00_0CA0(2, ab[22]),
@@ -123,4 +106,24 @@ const rack8 = {
   ]
 }
 
-module.exports = [rack1, rack2, rack3, rack4, rack5, rack6, rack7, rack8]
+const { Pn } = require('../../../models/Pn')
+
+const pn00 = new Pn('PLC', 0, { status: 1 }, 'CPU', { key: 'cpu', query: {} })
+const pn01 = new Pn('LS1', 1, merkers.find(b => b.addr === 'M4.0'), 'IM 155-5 PN ST', { key: 'im', query: {} })
+const pn02 = new Pn('KKP1', 2, merkers.find(b => b.addr === 'M4.1'), 'IM 155-6 PN ST', { key: 'im', query: {} })
+const pn03 = new Pn('SH1', 3, merkers.find(b => b.addr === 'M4.2'), 'IM 155-6 PN ST', { key: 'im', query: {} })
+const pn04 = new Pn('KKE1', 4, merkers.find(b => b.addr === 'M4.3'), 'IM 155-6 PN ST', { key: 'im', query: {} })
+const pn05 = new Pn('LS2', 5, merkers.find(b => b.addr === 'M4.4'), 'IM 155-5 PN ST', { key: 'im', query: {} })
+const pn06 = new Pn('KKP2', 6, merkers.find(b => b.addr === 'M4.5'), 'IM 155-6 PN ST', { key: 'im', query: {} })
+const pn07 = new Pn('SH2', 1, merkers.find(b => b.addr === 'M4.6'), 'IM 155-6 PN ST', { key: 'im', query: {} })
+
+pn00.rack = rack1
+pn01.rack = rack2
+pn02.rack = rack3
+pn03.rack = rack4
+pn04.rack = rack5
+pn05.rack = rack6
+pn06.rack = rack7
+pn07.rack = rack8
+
+module.exports = [pn00, pn01, pn02, pn03, pn04, pn05, pn06, pn07]
