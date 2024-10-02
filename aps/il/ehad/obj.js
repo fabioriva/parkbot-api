91f86c412dc14f1e19393b02162a4764daba6468
Fabio Riva <f.riva@sotefin.ch>
2024-10-02 15:37:44 +0100

    added alarm messages

--- aps/il/ehad/obj.js
+++ aps/il/ehad/obj.js
@@ -8,8 +8,8 @@ const { generateQueue } = require('../../../models/Queue')
 const { generateStalls } = require('../../../models/Stall')
 
 const al01 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(0, 64)), 1) // T
-const al02 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 2) // EL1
-const al03 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 3) // EL2
+const al02 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 2) // EU1
+const al03 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 3) // EU2
 exports.alarms = [al01, al02, al03]
 
 const inputs1 = generateBits('E', 0, 8, str.inputs1)
