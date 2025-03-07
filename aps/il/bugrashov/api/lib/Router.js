084b1474c9e239dc79b002d195bf9d54f18d6fda
Fabio Riva <f.riva@sotefin.ch>
2025-03-07 09:03:40 +0000

    api/bay route implementation

--- aps/il/bugrashov/api/lib/Router.js
+++ aps/il/bugrashov/api/lib/Router.js
@@ -23,24 +23,46 @@ class Router {
     // const prefix = "/interface/" + def.APS;
     const prefix = '/api'
     /* Endpoint /bay */
-    this.app.get(prefix + '/bay', (res, req) => {
+    this.app.get(prefix + '/bay', async (res, req) => {
       this.log(req)
       // const bay = Math.floor(Math.random() * 5) // Expected output: 0, 1, 2, 3 or 4
-      // const bay = 1 // force Bay 1 for now
-      // sendJson(res, { bay })
-      if (obj.overview.bays[0].status === 2) {
-        return sendJson(res, { bay: 1 })
+      // if (obj.overview.bays[0].status === 2) {
+      //   return sendJson(res, { bay: 1 })
+      // }
+      // if (obj.overview.bays[1].status === 2) {
+      //   return sendJson(res, { bay: 2 })
+      // }
+      // if (obj.overview.bays[2].status === 2) {
+      //   return sendJson(res, { bay: 3 })
+      // }
+      // if (obj.overview.bays[3].status === 2) {
+      //   return sendJson(res, { bay: 4 })
+      // }
+      // return sendJson(res, { bay: 0 })
+      res.onAborted(() => {
+        res.aborted = true
+      })
+      const bay = obj.next.bay
+      const { area, dbNumber, start, amount, wordLen } = def.REQ_BAY
+      const buffer = Buffer.allocUnsafe(amount)
+      buffer.writeUInt16BE(bay, 0)
+      const response = await writeArea(
+        this.plc.client,
+        area,
+        dbNumber,
+        start,
+        amount,
+        wordLen,
+        buffer
+      )
+      if (response) {
+        return sendJson(res, { bay })
+      } else {
+        return sendJson(
+          res,
+          new Message(SEVERITY.WARNING, MESG.BAY_NOT_VALID)
+        )
       }
-      if (obj.overview.bays[1].status === 2) {
-        return sendJson(res, { bay: 2 })
-      }
-      if (obj.overview.bays[2].status === 2) {
-        return sendJson(res, { bay: 3 })
-      }
-      if (obj.overview.bays[3].status === 2) {
-        return sendJson(res, { bay: 4 })
-      }
-      return sendJson(res, { bay: 0 })
     })
     /* Endpoint /overview */
     this.app.get(prefix + '/overview', (res, req) => {
