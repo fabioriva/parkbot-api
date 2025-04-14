c47aec8cd8f99ed442a35af30d526473c0a6d046
Fabio Riva <f.riva@sotefin.ch>
2025-04-14 14:31:12 +0100

    removed comments

--- aps/ch/smoritz/obj.js
+++ aps/ch/smoritz/obj.js
@@ -16,34 +16,6 @@ const al02 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 2) // 
 const al03 = new Alarms(generateAlarms(1, 64, str.ALARMS.slice(64, 128)), 3) // EL(R)
 export const alarms = [al01, al02, al03]
 
-// const inputs1 = generateBits('E', 4, 15, str.inputs1)
-// const inputs2 = generateBits('E', 28, 31, str.inputs2)
-// const inputs3 = generateBits('E', 32, 32, str.inputs3)
-// const inputs = inputs1.concat(inputs2, inputs3)
-// exports.inputs = inputs
-// const eb = generateBytes(inputs)
-// exports.eb = eb
-
-// const outputs1 = generateBits('A', 4, 11, str.outputs1)
-// const outputs2 = generateBits('A', 28, 29, str.outputs2)
-// const outputs3 = generateBits('A', 32, 32, str.outputs3)
-// const outputs = outputs1.concat(outputs2, outputs3)
-// exports.outputs = outputs
-// const ab = generateBytes(outputs)
-// exports.ab = ab
-
-// const racks = require('./racks')
-// exports.racks = racks
-
-// const merkers = generateBits('M', 0, 7)
-// exports.merkers = merkers
-// const mb = generateBytes(merkers)
-// exports.mb = mb
-
-// const device1 = require('./device1') // EL
-// const device2 = require('./device2') // EL(L)
-// const device3 = require('./device3') // EL(R)
-
 export const queue = generateQueue(def)
 
 export const devices = [
