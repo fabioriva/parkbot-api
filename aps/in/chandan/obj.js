8dba3b8fdc180b5570586e47ade568790f459683
Fabio Riva <f.riva@sotefin.ch>
2026-04-09 10:55:48 +0100

    fix: aps/in/chandan map layout

--- aps/in/chandan/obj.js
+++ aps/in/chandan/obj.js
@@ -76,6 +76,14 @@ export const cards = generateCards(def)
 
 export const stalls = generateStalls(def)
 
+const P3 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97, 100, 103] // 3rd basement SEDAN
+const P2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59, 62, 65, 68, 71, 74, 77, 80, 83, 86, 89, 92, 95, 98, 101, 104] // 2nd basement SUV
+const P1 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105] // 1st basement SEDAN
+
+const stallsP3 = stalls.filter((s) => P3.includes(s.nr))
+const stallsP2 = stalls.filter((s) => P2.includes(s.nr))
+const stallsP1 = stalls.filter((s) => P1.includes(s.nr))
+
 export const map = {
   definitions: {
     cards: def.CARDS,
@@ -88,7 +96,8 @@ export const map = {
       label: '3rd basement (P1)',
       min: 1,
       max: 35,
-      stalls: stalls.slice(0, 35),
+      // stalls: stalls.slice(0, 35),
+      stalls: stallsP1,
       elevators: [
         { id: 'el-1', label: 'EL1' },
         { id: 'el-2', label: 'EL2' }
@@ -99,7 +108,8 @@ export const map = {
       label: '2nd basement (P2)',
       min: 36,
       max: 70,
-      stalls: stalls.slice(35, 70),
+      // stalls: stalls.slice(35, 70),
+      stalls: stallsP2,
       elevators: [
         { id: 'el-1', label: 'EL1' },
         { id: 'el-2', label: 'EL2' }
@@ -110,7 +120,8 @@ export const map = {
       label: '1st basement (P3)',
       min: 71,
       max: 105,
-      stalls: stalls.slice(70, 105),
+      // stalls: stalls.slice(70, 105),
+      stalls: stallsP3,
       elevators: [
         { id: 'el-1', label: 'EL1' },
         { id: 'el-2', label: 'EL2' }
