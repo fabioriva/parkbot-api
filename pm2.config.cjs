b51dc48d1a85f38f6edff3bd41728fd1c8a95aa5
Fabio Riva <f.riva@sotefin.ch>
2026-04-29 07:38:07 +0100

    don't set max momory restart flag

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -49,7 +49,7 @@ module.exports = {
       name: 'api-muse',
       script: './aps/us/muse/index.js',
       cwd: './',
-      max_memory_restart: '150M',
+      // max_memory_restart: '150M',
       node_args: '-r dotenv/config',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       out_file: '/dev/null'
