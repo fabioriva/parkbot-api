12243bac2f11eb1deb6ab480af3e0149dab6de96
Fabio Riva <f.riva@sotefin.ch>
2026-04-29 07:32:59 +0100

    added muse backend service to pm2 ecosystem

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -46,6 +46,15 @@ module.exports = {
       out_file: '/dev/null'
     },
     {
+      name: 'api-muse',
+      script: './aps/us/muse/index.js',
+      cwd: './',
+      max_memory_restart: '150M',
+      node_args: '-r dotenv/config',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      out_file: '/dev/null'
+    },
+    {
       name: 'api-spire',
       script: './aps/us/spire/index.js',
       cwd: './',
