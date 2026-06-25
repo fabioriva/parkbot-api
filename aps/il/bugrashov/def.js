ba74cfdc7427d8b19222c56b37f595645262b980
Fabio Riva <f.riva@sotefin.ch>
2026-06-25 13:43:24 +0100

    set plc ip address for production

--- aps/il/bugrashov/def.js
+++ aps/il/bugrashov/def.js
@@ -6,7 +6,7 @@ export const HOST =
     : process.env.PRODUCTION_SERVER
 export const HTTP = 9038
 export const PLC = {
-  ip: '192.168.20.55', // 85.2',
+  ip: '192.168.85.2',
   rack: 0,
   slot: 1,
   polling_time: 500
