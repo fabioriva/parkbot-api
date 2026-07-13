61c307d84ff475ec0559dff605fa5886c8ee043f
Fabio Riva <f.riva@sotefin.ch>
2026-07-13 16:02:02 +0100

    disable plc tcp log

--- aps/in/kn/left/index.js
+++ aps/in/kn/left/index.js
@@ -5,7 +5,7 @@ import * as str from '../str.js'
 import obj from '../obj.js'
 import mongo from '../../../../lib/db.js'
 import History from '../../../../lib/History.js'
-import MailingList from '../../../../lib/MailingList.js'
+// import MailingList from '../../../../lib/MailingList.js'
 import Plc from '../../../../lib/Plc.js'
 import Router from '../../../../lib/Router.js'
 
@@ -15,16 +15,17 @@ const main = async () => {
     // const db = await mongo(def.APS, str)
     const db = await mongo('kn', str)
     const history = new History(db)
-    const mailingList = new MailingList(db)
-    const plc01 = new Plc(def.PLC)
-    plc01.on('log', async log => {
-      const doc = await history.saveLog(log)
-      mailingList.sendMail(def.APS, doc)
-      app.publish('aps/info', JSON.stringify({ notification: doc }))
-    })
-    plc01.on('pub', ({ channel, data }) => app.publish(channel, data))
+    // const mailingList = new MailingList(db)
+    const plc01 = new Plc(app, history)
+    // const plc01 = new Plc(def.PLC)
+    // plc01.on('log', async log => {
+    //   const doc = await history.saveLog(log)
+    //   mailingList.sendMail(def.APS, doc)
+    //   app.publish('aps/info', JSON.stringify({ notification: doc }))
+    // })
+    // plc01.on('pub', ({ channel, data }) => app.publish(channel, data))
     plc01.run(def, obj)
-    const router = new Router(app, history, mailingList, plc01)
+    const router = new Router(app, history, plc01)
     router.run(def, obj)
   } catch (err) {
     console.error(new Error(err))
