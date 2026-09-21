4308c5b0b710d9778390da4089b0c524bdaab161
Fabio Riva <f.riva@sotefin.ch>
2026-09-21 12:01:15 +0200

    use snap7-node-addon-api

--- aps/ae/daman/PlcW.js
+++ aps/ae/daman/PlcW.js
@@ -1,27 +1,38 @@
-import { EventEmitter } from 'events'
+// import { EventEmitter } from 'events'
 import pino from 'pino'
-import snap7 from 'node-snap7'
-import { ReadArea } from '../../../lib/utils7.js'
+// import snap7 from 'node-snap7'
+import Snap7Driver from '../../../lib/Snap7Driver.js'
+// import { ReadArea } from '../../../lib/utils7.js'
 
 const logger = pino()
 
-class PLC extends EventEmitter {
+// class PLC extends EventEmitter {
+class PLC {
   constructor (plc) {
-    super()
-    this.client = new snap7.S7Client()
+    // super()
+    // this.client = new snap7.S7Client()
+    this.client = new Snap7Driver()
+
     this.online = false
-    this.params = plc
+    // this.params = plc
   }
 
   async error (e) {
-    this.online = !this.client.Disconnect()
+    // this.online = !this.client.Disconnect()
+    try {
+      await this.client.Disconnect()
+      this.online = false
+    } catch (err) {
+      this.online = false
+    }
     isNaN(e) ? logger.error(e) : logger.error(this.client.ErrorText(e))
   }
 
   async read (def, obj) {
     try {
       const { area, dbNumber, start, amount, wordLen } = def.DATA_READ
-      this.online ? await ReadArea(this.client, area, dbNumber, start, amount, wordLen) : Buffer.alloc(amount)
+      // this.online ? await ReadArea(this.client, area, dbNumber, start, amount, wordLen) : Buffer.alloc(amount)
+      this.online ? await this.client.ReadArea(area, dbNumber, start, amount, wordLen) : Buffer.alloc(amount)
     } catch (e) {
       this.error(e)
     }
@@ -29,7 +40,12 @@ class PLC extends EventEmitter {
 
   async run (def, obj) {
     try {
-      this.online = this.client.ConnectTo(this.params.ip, this.params.rack, this.params.slot)
+      // this.online = this.client.ConnectTo(this.params.ip, this.params.rack, this.params.slot)
+      this.online = this.client.ConnectTo(
+        def.PLC.ip,
+        def.PLC.rack,
+        def.PLC.slot
+      )
       this.forever(def, obj)
     } catch (e) {
       this.error(e)
@@ -43,10 +59,12 @@ class PLC extends EventEmitter {
         this.read(def, obj)
       } else {
         this.online = this.client.Connect()
-        this.online ? logger.info('Connected to PLC %s', this.params.ip) : logger.info('Connecting to PLC %s ...', this.params.ip)
+        this.online
+          ? logger.info('Connected to PLC %s', def.PLC.ip)
+          : logger.info('Connecting to PLC %s ...', def.PLC.ip)
       }
       this.forever(def, obj)
-    }, this.params.polling_time)
+    }, def.PLC.polling_time)
   }
 
   stall (def, obj) {
